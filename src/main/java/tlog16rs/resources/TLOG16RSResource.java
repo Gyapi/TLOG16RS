40c068b7d0600127195728413fe1a99d0c9c4dc7
Gyapi <Gyapi@Gyapi-PC>
2017-12-15 16:35:37 +0100

    Greetings

--- src/main/java/tlog16rs/resources/TLOG16RSResource.java
+++ src/main/java/tlog16rs/resources/TLOG16RSResource.java
@@ -1,17 +1,31 @@
 package tlog16rs.resources;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
+@Produces(MediaType.TEXT_PLAIN)
 public class TLOG16RSResource {
     
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String getGreeting() {
         return "Hello world!";
+    }    
+    
+    @Path("/{name}")
+    @GET
+    public String getNamedGreeting(@PathParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
     
+    @Path("/query_param")
+    @GET
+    public String getNamedStringWithParam(@DefaultValue("world") @QueryParam("name") String name) {
+        return "Hello " + name;
+    }
 }
