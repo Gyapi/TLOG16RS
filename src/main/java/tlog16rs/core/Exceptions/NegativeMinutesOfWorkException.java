8659b8c769076d92b5b948dcfacf053b4c58a723
Gyapi <Gyapi@Gyapi-PC>
2018-01-03 17:02:26 +0100

    Adding TLOG classes

---
+++ src/main/java/tlog16rs/core/Exceptions/NegativeMinutesOfWorkException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tlog16rs.core.Exceptions;
+
+/**
+ *
+ * @author Gyapi
+ */
+public class NegativeMinutesOfWorkException extends Exception{
+
+    public NegativeMinutesOfWorkException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/tlog16rs/core/Exceptions/NotExpectedTimeOrderException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tlog16rs.core.Exceptions;
+
+/**
+ *
+ * @author Gyapi
+ */
+public class NotExpectedTimeOrderException extends Exception{
+
+    public NotExpectedTimeOrderException(String message) {
+        super(message);
+    }
+    
+}
